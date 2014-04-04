de013466a4e95093e6fdc68d49c4af92a4c140ec
user <user@user-desktop.(none)>
2014-04-04 19:22:19 +0530

    added changes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,6 @@
 class Job < ActiveRecord::Base
    has_many :job_emails, :dependent => :destroy
+   validates_presence_of :name
+   validates_presence_of :start_date
+   validates_presence_of :end_date
 end
